b2e016b16acb84b4fb3a4d783163bb7aebd7aee9
Léo FASANO <leo.fasano@cognitives-labs.com>
2022-04-03 16:13:11 +0200

    added "WRITE_VAR" function to byteCode impl

--- src/entities/byteCode.rs
+++ src/entities/byteCode.rs
@@ -26,4 +26,24 @@ impl ByteCode {
     pub fn LOAD_VAL(&mut self, value: f64){
         self.stack.push(value);
     }
+
+    pub fn WRITE_VAR(&mut self, var_name: String){
+        let value: f64 = self.stack[self.stack.len()-1];
+        let index = self.environment
+            .iter()
+            .position(|r| r.0 == var_name.clone())
+            .unwrap_or(
+                std::u64::MAX.try_into().unwrap()
+            );
+
+        if index == std::u64::MAX.try_into().unwrap() {
+            self.environment.push((var_name, value));
+            println!("new variable {} = {}", self.environment[self.environment.len()-1].0, self.environment[self.environment.len()-1].1)
+        } 
+        else {
+            self.environment[index].1 = value;
+            println!("Assign variable {} = {}", self.environment[self.environment.len()-1].0, self.environment[self.environment.len()-1].1)
+        }
+        self.pop();
+    }
 }
