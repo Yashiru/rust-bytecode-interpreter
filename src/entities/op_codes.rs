d13309c61b76b2842f70fae737c0712551f2201b
Léo FASANO <leo.fasano@cognitives-labs.com>
2022-04-04 00:21:20 +0200

    added loop features operation codes in OpCodes enum

--- src/entities/op_codes.rs
+++ src/entities/op_codes.rs
@@ -7,31 +7,49 @@ pub enum OpCodes {
     Add,
     Multiply,
     ReturnValue,
+    TestLessThan,
+    TestMoreThan,
+    TestEqualsTo,
+    TestDifferentFrom,
+    JumpIfFalse,
+    JumpLoop,
 }
 
 
 impl fmt::Display for OpCodes {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-       match *self {
-           OpCodes::LoadVal => write!(f, "0"),
-           OpCodes::WriteVar => write!(f, "1"),
-           OpCodes::ReadVar => write!(f, "2"),
-           OpCodes::Add => write!(f, "3"),
-           OpCodes::Multiply => write!(f, "4"),
-           OpCodes::ReturnValue => write!(f, "5"),
-       }
+        match *self {
+            OpCodes::LoadVal => write!(f, "0"),
+            OpCodes::WriteVar => write!(f, "1"),
+            OpCodes::ReadVar => write!(f, "2"),
+            OpCodes::Add => write!(f, "3"),
+            OpCodes::Multiply => write!(f, "4"),
+            OpCodes::ReturnValue => write!(f, "5"),
+            OpCodes::TestLessThan => write!(f, "6"),
+            OpCodes::TestMoreThan => write!(f, "7"),
+            OpCodes::TestEqualsTo => write!(f, "8"),
+            OpCodes::TestDifferentFrom => write!(f, "9"),
+            OpCodes::JumpIfFalse => write!(f, "a"),
+            OpCodes::JumpLoop => write!(f, "b")
+        }
     }
 }
 
 impl fmt::Debug for OpCodes {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-       match *self {
-           OpCodes::LoadVal => write!(f, "0"),
-           OpCodes::WriteVar => write!(f, "1"),
-           OpCodes::ReadVar => write!(f, "2"),
-           OpCodes::Add => write!(f, "3"),
-           OpCodes::Multiply => write!(f, "4"),
-           OpCodes::ReturnValue => write!(f, "5"),
-       }
+        match *self {
+            OpCodes::LoadVal => write!(f, "0"),
+            OpCodes::WriteVar => write!(f, "1"),
+            OpCodes::ReadVar => write!(f, "2"),
+            OpCodes::Add => write!(f, "3"),
+            OpCodes::Multiply => write!(f, "4"),
+            OpCodes::ReturnValue => write!(f, "5"),
+            OpCodes::TestLessThan => write!(f, "6"),
+            OpCodes::TestMoreThan => write!(f, "7"),
+            OpCodes::TestEqualsTo => write!(f, "8"),
+            OpCodes::TestDifferentFrom => write!(f, "9"),
+            OpCodes::JumpIfFalse => write!(f, "a"),
+            OpCodes::JumpLoop => write!(f, "b")
+        }
     }
 }
