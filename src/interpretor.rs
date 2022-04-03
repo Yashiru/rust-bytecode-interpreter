31f4b83ac43467fc471e66a55b609164f711690f
Léo FASANO <leo.fasano@cognitives-labs.com>
2022-04-03 22:51:16 +0200

    replaced operator name interpretation by operator code interpretation

--- src/interpretor.rs
+++ src/interpretor.rs
@@ -1,49 +1,51 @@
 #[path = "entities/byte_code.rs"] mod byte_code;
+#[path = "entities/op_codes.rs"] mod op_codes;
 
 pub struct Interpretor {
     pub instruction_pointer: u32,
-    pub instructions: Vec<(String, String)>,
+    pub instructions: Vec<(op_codes::OpCodes, String)>,
     pub byte_code: byte_code::ByteCode
 }
 
 impl Interpretor{
     fn process_instruction(&mut self, instruction_index: usize){
         let oparg: &str = &self.instructions[instruction_index].1[..];
-        match &self.instructions[instruction_index].0[..] {
-            "LOAD_VAL"  => self.byte_code.load_val(oparg),
-            "WRITE_VAR" => self.byte_code.write_var(oparg),
-            "READ_VAR" => self.byte_code.read_var(oparg),
-            "ADD" => self.byte_code.add(),
-            "MULTIPLY" => self.byte_code.multiply(),
-            "RETURN_VALUE" => {
+
+        match self.instructions[instruction_index].0 {
+            op_codes::OpCodes::LoadVal => self.byte_code.load_val(oparg),
+            op_codes::OpCodes::WriteVar => self.byte_code.write_var(oparg),
+            op_codes::OpCodes::ReadVar => self.byte_code.read_var(oparg),
+            op_codes::OpCodes::Add => self.byte_code.add(),
+            op_codes::OpCodes::Multiply => self.byte_code.multiply(),
+            op_codes::OpCodes::ReturnValue => {
                 println!(
                     "================================\n\x1b[36mValue returned from the stack: {}\x1b[0m\n================================", 
                     self.byte_code.return_value()
                 )
-            },
-            _ => println!("Unrecognized opname")
+            }
         }
     }
 
-    pub fn new(instructions: Vec<(String, String)>) -> Interpretor {
+    pub fn new(instructions: Vec<(op_codes::OpCodes, String)>) -> Interpretor {
         Interpretor {
             instruction_pointer: 0,
             instructions: instructions,
             byte_code: byte_code::ByteCode::new([].to_vec())
         }
+
     }
 
     pub fn interpret(&mut self) {
-
-        for i in 0..self.instructions.len() {
+        self.instruction_pointer = 0;
+        for _i in 0..self.instructions.len() {
+            self.process_instruction(self.instruction_pointer.try_into().unwrap());
             self.instruction_pointer = self.instruction_pointer + 1;
-            self.process_instruction(i)
         }
     }
 }
 
-pub fn text_to_operations(text: &str) -> Vec<(String, String)>{
-    let mut operations: Vec<(String, String)> = vec![];
+pub fn text_to_operations(text: &str) -> Vec<(op_codes::OpCodes, String)>{
+    let mut operations: Vec<(op_codes::OpCodes, String)> = vec![];
     let full_operations: Vec<&str> = text.trim().split("\n").collect();
     
     for i in 0..full_operations.len() {
@@ -56,12 +58,26 @@ pub fn text_to_operations(text: &str) -> Vec<(String, String)>{
         if operation.len() == 1 {
             operation.push("")
         }
+        
+        let op_code;
+        match operation[0] {
+            "LOAD_VAL" => op_code = op_codes::OpCodes::LoadVal,
+            "WRITE_VAR" => op_code = op_codes::OpCodes::WriteVar,
+            "READ_VAR" => op_code = op_codes::OpCodes::ReadVar,
+            "ADD" => op_code = op_codes::OpCodes::Add,
+            "MULTIPLY" => op_code = op_codes::OpCodes::Multiply,
+            "RETURN_VALUE" => op_code = op_codes::OpCodes::ReturnValue,
+            _ => panic!("Unrecognized opname")
+        }
+
+
         operations.push(
             (
-                String::from(operation[0]),
+                op_code,
                 String::from(operation[1])
             )
         );
+
     }
 
     operations
