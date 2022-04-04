eef8747251c914725e3a4ef70fc85d3d98a5becc
Léo FASANO <leo.fasano@cognitives-labs.com>
2022-04-04 13:53:24 +0200

    added main.rs input file to interpret a bytecode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+mod interpretor;
+
 fn main() {
-    println!("Hello, world!");
+    let mut interpretor = interpretor::Interpretor::new(
+        interpretor::text_to_operations("
+            LOAD_VAL 1
+            WRITE_VAR 'x'
+            LOAD_VAL 2
+            WRITE_VAR 'y'
+            READ_VAR 'x'
+            LOAD_VAL 1
+            ADD
+            READ_VAR 'y'
+            MULTIPLY
+            RETURN_VALUE
+            LOAD_VAL 0
+            WRITE_VAR 'x'
+            LOAD_VAL 0
+            WRITE_VAR 'i'
+            READ_VAR 'i'
+            TEST_DIFFERENT_FROM 5
+            JUMP_IF_FALSE 26
+            READ_VAR 'x'
+            LOAD_VAL 10
+            ADD
+            WRITE_VAR 'x'
+            READ_VAR 'i'
+            LOAD_VAL 1
+            ADD
+            WRITE_VAR 'i'
+            JUMP_LOOP 14
+            READ_VAR 'x'
+            RETURN_VALUE
+        ")
+    );
+
+    interpretor.interpret();
 }
